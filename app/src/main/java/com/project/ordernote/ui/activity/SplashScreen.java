acc9cb5d0b38ef9292dc4f182309cfee19ec3bd2
SAC <sacglobetech@gmail.com>
2024-07-13 10:06:54 +0530

    Add detailed message about what was changed

--- app/src/main/java/com/project/ordernote/ui/activity/SplashScreen.java
+++ app/src/main/java/com/project/ordernote/ui/activity/SplashScreen.java
@@ -2,17 +2,26 @@ package com.project.ordernote.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
 import com.project.ordernote.R;
 
 public class SplashScreen extends AppCompatActivity {
-
+    private static final int SPLASH_DISPLAY_LENGTH = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
-        Toast.makeText(this, "HI", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, Dashboard.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
     }
 }
